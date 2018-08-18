e8f0c46d5046b6f2e71111aca6d4a7f4f32269eb
yueshuchen <32773383+yueshuchen@users.noreply.github.com>
2018-08-18 19:25:15 -0400

    recommendation implement

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -13,8 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import algorithm.GeoRecommendation;
 import db.DBConnection;
 import db.DBConnectionFactory;
+import entity.Item;
 
 /**
  * Servlet implementation class RecommendItem
@@ -36,23 +38,22 @@ public class RecommendItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		DBConnection connection = DBConnectionFactory.getConnection();
-	   	 try {
-	   		 JSONObject input = RpcHelper.readJSONObject(request);
-	   		 String userId = input.getString("user_id");
-	   		 JSONArray array = input.getJSONArray("favorite");
-	   		 List<String> itemIds = new ArrayList<>();
-	   		 for(int i = 0; i < array.length(); ++i) {
-	   			 itemIds.add(array.getString(i));
-	   		 }
-	   		 connection.setFavoriteItems(userId, itemIds);
-	   		 RpcHelper.writeJsonObject(response, new JSONObject().put("result", "SUCCESS"));
-	   		 
-	   	 } catch (Exception e) {
-	   		 e.printStackTrace();
-	   	 } finally {
-	   		 connection.close();
-	   	 }
+		String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		GeoRecommendation recommendation = new GeoRecommendation();
+		List<Item> items = recommendation.recommendItems(userId, lat, lon);
+
+		JSONArray result = new JSONArray();
+		try {
+			for (Item item : items) {
+				result.put(item.toJSONObject());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		RpcHelper.writeJsonArray(response, result); 
+
 
 	}
 
