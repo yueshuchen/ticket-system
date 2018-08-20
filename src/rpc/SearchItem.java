575e44142cdd560d717509e4cf279c9e75a2d569
yueshuchen <32773383+yueshuchen@users.noreply.github.com>
2018-08-19 23:51:08 -0400

    search add userid

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import db.DBConnection;
@@ -38,23 +40,28 @@ public class SearchItem extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
+		String term = request.getParameter("term");
+		String userId = request.getParameter("user_id");
 		
-		// term can be empty
-		String keyword = request.getParameter("term");
 		DBConnection connection = DBConnectionFactory.getConnection();
 		try {
-			List<Item> items = connection.searchItems(lat, lon, keyword);
+			List<Item> items = connection.searchItems(lat, lon, term);
+			Set<String> favoriteItems = connection.getFavoriteItemIds(userId);
+
 			JSONArray array = new JSONArray();
 			for (Item item : items) {
 				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoriteItems.contains(item.getItemId()));
+
 				array.put(obj);
 			}
 			RpcHelper.writeJsonArray(response, array);
-		} catch (Exception e) {
+		} catch (JSONException e) {
 			e.printStackTrace();
 		} finally {
 			connection.close();
 		}
+
 		
 
 	}
